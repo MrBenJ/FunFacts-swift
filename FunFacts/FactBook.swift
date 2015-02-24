eff215610835b43d4023e8aa2a754e13cc186174
Ben Junya <bjunya@gmail.com>
2015-02-23 18:30:45 -0800

    Added FactBook model. Added IBAction and IBOutlet to button and label

---
+++ FunFacts/FactBook.swift
@@ -0,0 +1,24 @@
+//
+//  FactBook.swift
+//  FunFacts
+//
+//  Created by Ben Junya on 2/23/15.
+//  Copyright (c) 2015 Prism-Mobile. All rights reserved.
+//
+
+import Foundation
+
+struct FactBook {
+    let factsArray = [
+        "Ants stretch when they wake up in the morning",
+        "Ostriches can run faster than horses.",
+        "Olympic gold medals are actually mostly made of silver.",
+        "You are born with 300 bones; by the time you are an adult, you will have 206",
+        "It takes about 8 minutes for light from the Sun to reach Earth",
+        "Some bamboo plants can grow almost a meter in just one day.",
+        "some penguins can leap 2-3 meters out of the water.",
+        "The state of Florida is bigger than England.",
+        "On average, it takes 66 days to form a new habit.",
+        "Mammoths still walked the earth when the Great Pyramid was being built.",
+    ]
+}
--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -3,15 +3,18 @@
 //  FunFacts
 //
 //  Created by Ben Junya on 2/20/15.
-//  Copyright (c) 2015 Prism-Mobile. All rights reserved.
+//  Copyright (c) 2015 Prism Mobile. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var funFactLabel: UILabel!
+    let factBook = FactBook()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        funFactLabel.text = factBook.factsArray[0]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +22,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // onClick
+    @IBAction func showFunFact() {
+        
+        funFactLabel.text = factBook.factsArray[1]
+    }
+    
 
 
 }
